6541cb2b7291bd24470ea80e839277b7775ea7f7
hklknz <haikalluthfianinobaluqia598@gmail.com>
2022-12-21 16:52:31 +0000

    KEBELET xdCAF
    signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- xdroid_tissot.mk
+++ xdroid_tissot.mk
@@ -22,18 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nad/config/common_full_phone.mk)
-$(call inherit-product-if-exists, packages/apps/NusantaraParts/nadproject.mk)
-NAD_BUILD_TYPE := OFFICIAL
+$(call inherit-product, vendor/xdroid/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-IS_PHONE := true
+XDROID_BOOT_LIGHT := true
+XDROID_UI_BLUR := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := nad_tissot
+PRODUCT_NAME := xdroid_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
