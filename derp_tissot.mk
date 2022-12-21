d8e6ea5d98e459b430a2ea5499d12a2e5dc81329
hklknz <haikalluthfianinobaluqia598@gmail.com>
2022-12-21 15:24:56 +0000

    derpnesss

--- derp_tissot.mk
+++ derp_tissot.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common xdroid stuff.
-$(call inherit-product, vendor/xdroid/config/common.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := xdroid_tissot
+PRODUCT_NAME := derp_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -41,9 +41,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-# xdroid Stuffs
-XDROID_BOOT := 1080
-XDROID_MAINTAINER := HonokaKousaka17
+# derpness
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
